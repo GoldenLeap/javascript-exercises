b52ee52ec3b688b590bf810fc090637c45790a91
GoldenLeap <115042820+GoldenLeap@users.noreply.github.com>
2025-03-05 17:28:06 -0300

    Solve exercises 01-03:
    01 - Added return statement 'Hello, World!'
    02 - Added logic to repeatString function
    03 - Added logic to reverseString function

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    let text = ''; // Initialize an empty string
+    if(num < 0){ // Return 'ERROR' if num is negative
+        return 'ERROR';
+    }
+    for(i = 0; i < num; i++){ // Repeat the string 'num' times
+        text += str;
+    }
+    return text; // Return repeated string
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let reverseStr = ''; // Initialize an empty string
+    for(let i = str.length -1; i >= 0; i--){ // Loop through characters from end to start
+        reverseStr+=str[i];
+    }
+    return reverseStr; // Return the reversed string
 };
 
 // Do not edit below this line
